2122bf3be4be2fe7449bc257a6af6ec299bc4cab
TullioTambone <tulliotambone97@gmail.com>
2023-05-05 13:33:09 +0200

    crazione store

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,30 @@
 <script>
 import axios from 'axios';
 import HeaderComp from './components/HeaderComp.vue';
+import {store} from './store'
 
 export default{
     name:'App',
     components: {
         HeaderComp,
     },
-    created(){
-
-    },
     data(){
-
+        return{
+            store
+        }
     },
+    created(){
+        this.chiamataApi()
+    },
+    methods: {
+        chiamataApi(){
+            axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?archetype=Alien')
+            .then( res =>{
+                const datiApi = res.data
+                this.store.arrayCard = datiApi
+            })
+        }
+    }
     
 }
 </script>
