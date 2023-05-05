f54d6fc2b05743376b7e8a9d52e41c65b257c3dc
TullioTambone <tulliotambone97@gmail.com>
2023-05-05 14:59:52 +0200

    fine

--- src/components/SingolaCarta.vue
+++ src/components/SingolaCarta.vue
@@ -7,11 +7,11 @@ export default{
 </script>
 
 <template>
-    <div class="my-card col-12 col-md-4 col-lg-2">
+    <div class="my-card col-12 col-md-3 col-lg-2">
         <div class="col-12">
             <img :src="nomeCard.card_images[0].image_url" alt="">
         </div>
-        <h5>{{ nomeCard.name }}</h5>
+        <h5>{{ nomeCard.name.toUpperCase() }}</h5>
         <span>{{ nomeCard.archetype }}</span>
     </div>
 </template>
@@ -19,15 +19,16 @@ export default{
 <style lang="scss" scoped>
 .my-card{
     background-color: #d48f38;
-    border: 1px solid red;
     color: black;
     text-align: center;
-    div{
-        
-        img{
-            width: 100%;
-            margin-bottom: 5px;
-        }
+    padding: 0;
+    h5{
+        font-size: 15px;
+        color: white;
+    }
+    img{
+        width: 100%;
+        margin-bottom: 5px;
     }
 }
 </style>
